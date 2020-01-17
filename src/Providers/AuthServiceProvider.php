4bf903345178bf0c33a4b4227c433ea6c28172aa
Thomas Alrek <thomas@alrek.no>
2020-01-17 20:34:20 +0100

    Implement AuthServiceProvider

---
+++ src/Providers/AuthServiceProvider.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Netflex\Customers\Providers;
+
+use Netflex\Customers\Customer;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider implements UserProvider
+{
+  /**
+   * Register any application authentication / authorization services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    Auth::provider('netflex', function ($app, array $config) {
+      return new static($app);
+    });
+  }
+
+  /**
+   * Retrieve a user by their unique identifier.
+   *
+   * @param  mixed  $identifier
+   * @return \Illuminate\Contracts\Auth\Authenticatable|null
+   */
+  public function retrieveById($identifier)
+  {
+    return Customer::retrieve($identifier);
+  }
+
+  /**
+   * Retrieve a user by their unique identifier and "remember me" token.
+   *
+   * @param  mixed  $identifier
+   * @param  string  $token
+   * @return \Illuminate\Contracts\Auth\Authenticatable|null
+   */
+  public function retrieveByToken($identifier, $token)
+  {
+  }
+
+  /**
+   * Update the "remember me" token for the given user in storage.
+   *
+   * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+   * @param  string  $token
+   * @return void
+   */
+  public function updateRememberToken(Authenticatable $user, $token)
+  {
+  }
+
+  /**
+   * Retrieve a user by the given credentials.
+   *
+   * @param  array  $credentials
+   * @return \Illuminate\Contracts\Auth\Authenticatable|null
+   */
+  public function retrieveByCredentials(array $credentials)
+  {
+    if (
+      empty($credentials) ||
+      (count($credentials) === 1 &&
+        array_key_exists('password', $credentials))
+    ) {
+      return;
+    }
+
+    return Customer::resolve($credentials);
+  }
+
+  /**
+   * Validate a user against the given credentials.
+   *
+   * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+   * @param  array  $credentials
+   * @return bool
+   */
+  public function validateCredentials(Authenticatable $user, array $credentials)
+  {
+    return !!(Customer::authenticate($credentials));
+  }
+}
